805e96f49e84a46afaa1b72697215e4fbc619f28
vinosuryamts <vinosuryamts@outlook.com>
2019-10-23 18:11:56 +0530

    sta

--- datadrivenframework/src/TestSuite/Test1.java
+++ datadrivenframework/src/TestSuite/Test1.java
@@ -50,6 +50,9 @@ public class Test1 {
 					
 					LaunchBrowser.navigateto(driver, urlvalue);
 					
+
+					LaunchBrowser.implicitWait( driver,5, TimeUnit.SECONDS);
+					
 					try {
 						Thread.sleep(1000);
 					} catch (InterruptedException e) {
